fb97119d000301976ad2315f940b4b91cec746a8
Lenher <axel.masset@ynov.com>
2017-01-04 09:58:32 +0100

    Add BDD

---
+++ ProjetWpf/BDDWpf/Class1.cs
@@ -0,0 +1,53 @@
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDWpf
+{
+    public class Class1
+    {
+        SqliteConnection Connection;
+        string connectionString = "URI=file:Games.db";
+        SqliteCommand Command; 
+
+        public bool Open()
+        {
+            Connection = new SqliteConnection(connectionString);
+            Connection.Open();
+
+            Command = Connection.CreateCommand();
+            Command.CommandText = "CREATE / INSERT";
+            Command.ExecuteNonQuery();
+
+
+            Command.Dispose();
+
+            Command = Connection.CreateCommand();
+            Command.CommandText = "SELECT";
+            SqliteDataReader reader = Command.ExecuteReader();
+
+            while (reader.Read())
+            {
+
+                // récupération de la 2ème colonne
+                reader.GetString(1);
+            }
+
+
+            reader.Close();
+            Command.Dispose();
+            Connection.Close();
+
+            return true;
+            
+
+        }
+
+
+
+           
+    }
+}

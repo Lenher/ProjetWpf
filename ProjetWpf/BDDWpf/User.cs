83bb48e7948056803a60deea7c381a74e316c097
Nath33 <nathan.prevost@ynov.com>
2017-01-04 11:44:54 +0100

    Creation de la BDD

---
+++ ProjetWpf/BDDWpf/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetWpf
+{
+    public class User
+    {
+        public string Pseudo { get; set; }
+        public int Score { get; set; }
+
+        public User(string pseudo, int score)
+        {
+            Pseudo = pseudo;
+            Score = score;
+        }
+    }
+
+}

a34e76d944f8649d2a265ff0da2754c252758f06
Nath33 <nathan.prevost@ynov.com>
2017-01-10 08:10:16 +0100

    suppression + ajout BDDWpf

---
+++ BDDWpf/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDWpf
+{
+    public class User
+    {
+        public string Pseudo { get; set; }
+        public int Score { get; set; }
+
+        public User(string pseudo, int score)
+        {
+            Pseudo = pseudo;
+            Score = score;
+        }
+    }
+}
---
+++ BDDWpf/UserManager.cs
@@ -0,0 +1,61 @@
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDWpf
+{
+    public class UserManager
+    {
+        public string pseudo;
+        public int score;
+
+        SqliteConnection Connection;
+        string connectionString = "URI=file:scoringBoard.db";
+        SqliteCommand Command;
+
+        public void Open()
+        {
+            Connection = new SqliteConnection(connectionString);
+            Connection.Open();
+
+            Command = Connection.CreateCommand();
+            Command.CommandText = "CREATE TABLE IF NOT EXIST score(pseudo VARCHAR(100), score INT)";
+            Command.ExecuteNonQuery();
+
+            Command.Dispose();
+        }
+
+        public bool add(string pseudo, int score)
+        {
+            Command = Connection.CreateCommand();
+            Command.CommandText = "INSERT INTO score(pseudo, score) VALUE(" + pseudo + "," + score + ")";
+            int insertLines = Command.ExecuteNonQuery();
+            Command.Dispose();
+            return insertLines > 0;
+        }
+
+        public List<User> getByScore()
+        {
+            List<User> scoringBoard = new List<User>();
+            Command = Connection.CreateCommand();
+            Command.CommandText = "SELECT pseudo, score FROM scoringBoard ORDER BY score DESC LIMIT 5";
+            SqliteDataReader reader = Command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    scoringBoard.Add(new User(reader.GetString(0), reader.GetInt32(1)));
+                }
+            }
+
+            reader.Close();
+            Command.Dispose();
+
+            return scoringBoard;
+        }
+
+    }
+}
